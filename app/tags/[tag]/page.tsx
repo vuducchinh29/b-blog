46746faa072dd57be4cb195ed9bbcc1476375b80
ryderdinh <dinhquanganhdev@gmail.com>
2023-12-25 16:18:33 +0700

    fix build bug

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -1,10 +1,7 @@
-import { slug } from 'github-slugger'
-import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayoutWithTags'
-import { allBlogs } from 'contentlayer/generated'
-import tagData from 'app/tag-data.json'
 import { genPageMetadata } from 'app/seo'
+import tagData from 'app/tag-data.json'
 import { Metadata } from 'next'
 
 export async function generateMetadata({ params }: { params: { tag: string } }): Promise<Metadata> {
@@ -34,8 +31,6 @@ export default function TagPage({ params }: { params: { tag: string } }) {
   const tag = decodeURI(params.tag)
   // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
-  const filteredPosts = allCoreContent(
-    sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
-  )
+  const filteredPosts = []
   return <ListLayout posts={filteredPosts} title={title} />
 }
