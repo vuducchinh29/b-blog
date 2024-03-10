a6e90cdc080f5ce3826f36544604146fb00c6bf8
ryderdinh <dinhquanganhdev@gmail.com>
2024-03-10 21:34:09 +0700

    add op page

---
+++ components/op/footer.tsx
@@ -0,0 +1,81 @@
+import IconLogo from 'app/assets/images/icon-logo.webp'
+import Image from 'next/image'
+
+const Footer = () => {
+  return (
+    <div className="my-6 flex w-full flex-col items-center space-y-4">
+      <a className="group relative w-[500px] max-w-full" href=''>
+        <div
+          className="relative z-[2] flex w-[calc(100%-10px)] items-center justify-center rounded-full border-2 border-black bg-blue-600 px-2 py-4 font-semibold 
+          text-white group-hover:left-1.5 group-hover:top-1.5"
+        >
+          <Image
+            src={IconLogo}
+            alt="logo"
+            className="absolute left-2 top-1/2 h-14 w-14 -translate-y-1/2 object-contain object-center"
+          />
+          Website
+        </div>
+        <div className="absolute right-0 top-2.5 z-[1] h-full w-[calc(100%-10px)] rounded-full bg-black"></div>
+      </a>
+      <a className="group relative w-[500px] max-w-full" href=''>
+        <div
+          className="relative z-[2] flex w-[calc(100%-10px)] items-center justify-center rounded-full border-2 border-black bg-blue-600 px-2 py-4 font-semibold 
+          text-white group-hover:left-1.5 group-hover:top-1.5"
+        >
+          <Image
+            src={IconLogo}
+            alt="logo"
+            className="absolute left-2 top-1/2 h-14 w-14 -translate-y-1/2 object-contain object-center"
+          />
+          X
+        </div>
+        <div className="absolute right-0 top-2.5 z-[1] h-full w-[calc(100%-10px)] rounded-full bg-black"></div>
+      </a>
+      <a className="group relative w-[500px] max-w-full" href=''>
+        <div
+          className="relative z-[2] flex w-[calc(100%-10px)] items-center justify-center rounded-full border-2 border-black bg-blue-600 px-2 py-4 font-semibold 
+          text-white group-hover:left-1.5 group-hover:top-1.5"
+        >
+          <Image
+            src={IconLogo}
+            alt="logo"
+            className="absolute left-2 top-1/2 h-14 w-14 -translate-y-1/2 object-contain object-center"
+          />
+          Global Channel
+        </div>
+        <div className="absolute right-0 top-2.5 z-[1] h-full w-[calc(100%-10px)] rounded-full bg-black"></div>
+      </a>
+      <a className="group relative w-[500px] max-w-full" href=''>
+        <div
+          className="relative z-[2] flex w-[calc(100%-10px)] items-center justify-center rounded-full border-2 border-black bg-blue-600 px-2 py-4 font-semibold 
+          text-white group-hover:left-1.5 group-hover:top-1.5"
+        >
+          <Image
+            src={IconLogo}
+            alt="logo"
+            className="absolute left-2 top-1/2 h-14 w-14 -translate-y-1/2 object-contain object-center"
+          />
+          Group Chat
+        </div>
+        <div className="absolute right-0 top-2.5 z-[1] h-full w-[calc(100%-10px)] rounded-full bg-black"></div>
+      </a>
+      <a className="group relative w-[500px] max-w-full" href=''>
+        <div
+          className="relative z-[2] flex w-[calc(100%-10px)] items-center justify-center rounded-full border-2 border-black bg-blue-600 px-2 py-4 font-semibold 
+          text-white group-hover:left-1.5 group-hover:top-1.5"
+        >
+          <Image
+            src={IconLogo}
+            alt="logo"
+            className="absolute left-2 top-1/2 h-14 w-14 -translate-y-1/2 object-contain object-center"
+          />
+          Facebook
+        </div>
+        <div className="absolute right-0 top-2.5 z-[1] h-full w-[calc(100%-10px)] rounded-full bg-black"></div>
+      </a>
+    </div>
+  )
+}
+
+export default Footer
