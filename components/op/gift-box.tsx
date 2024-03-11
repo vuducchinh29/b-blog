a1f932f79db68b7793b6b44aae622a47d9fb14ed
vuducchinh29 <vuducchinh29@gmail.com>
2024-03-11 14:24:58 +0700

    add demo

--- components/op/gift-box.tsx
+++ components/op/gift-box.tsx
@@ -1,29 +1,31 @@
 'use client'
 
-import { useCallback, useReducer, useState } from 'react'
+import { useCallback, useEffect, useReducer, useState } from 'react'
 
 import boxLid from 'app/assets/images/box-lid.png'
 import box from 'app/assets/images/box.png'
 import Image from 'next/image'
 import Confetti from './confetti'
-
+import DemoGift from 'app/assets/images/demo-gift.jpeg'
+import {QRS} from './QR'
+import { useRouter } from 'next/navigation'
 interface Gift {
   name: string
-  image: string
+  image: any
   probability: number
 }
 
 const gifts: Gift[] = [
-  { name: 'Quà 1', image: '/path/to/image1.png', probability: 2 },
-  { name: 'Quà 2', image: '/path/to/image2.png', probability: 1 },
-  { name: 'Quà 3', image: '/path/to/image3.png', probability: 1 },
-  { name: 'Quà 4', image: '/path/to/image4.png', probability: 1 },
-  { name: 'Quà 5', image: '/path/to/image5.png', probability: 1 },
-  { name: 'Quà 6', image: '/path/to/image6.png', probability: 1 },
-  { name: 'Quà 7', image: '/path/to/image7.png', probability: 1 },
-  { name: 'Quà 8', image: '/path/to/image8.png', probability: 0.5 },
-  { name: 'Quà 9', image: '/path/to/image9.png', probability: 0.5 },
-  { name: 'Quà 10', image: '/path/to/image10.png', probability: 1 },
+  { name: 'Quà 1', image: DemoGift, probability: 2 },
+  { name: 'Quà 2', image: DemoGift, probability: 1 },
+  { name: 'Quà 3', image: DemoGift, probability: 1 },
+  { name: 'Quà 4', image: DemoGift, probability: 1 },
+  { name: 'Quà 5', image: DemoGift, probability: 1 },
+  { name: 'Quà 6', image: DemoGift, probability: 1 },
+  { name: 'Quà 7', image: DemoGift, probability: 1 },
+  { name: 'Quà 8', image: DemoGift, probability: 0.5 },
+  { name: 'Quà 9', image: DemoGift, probability: 0.5 },
+  { name: 'Quà 10', image: DemoGift, probability: 1 },
 ]
 
 const init_state = {
@@ -33,8 +35,9 @@ const init_state = {
   rotating: '',
 }
 export default function GiftBoxAnimation() {
+  const router = useRouter()
   const [isDone, setIsDone] = useState<boolean>(false)
-  const [gift, setGift] = useState({ name: 'Quà 3', image: '', probability: 1 })
+  const [gift, setGift] = useState({ name: '', image: undefined, probability: 1 })
   const [state, setState] = useReducer(
     (state, new_state) => ({
       ...state,
@@ -98,20 +101,33 @@ export default function GiftBoxAnimation() {
     return gifts[0] // Fallback nếu có lỗi
   }
 
+  useEffect(() => {
+    const queryString = window.location.search
+    const query = queryString.substring(1)
+    const qr = QRS.find((e) => e.includes(query))
+    console.log(qr)
+    if (!qr || qr?.length === 0) {
+      router.push('/') // back to home page
+    }
+  }, []);
+
   return (
     <div className="relative h-52 w-full">
       <Confetti open={jump === 'jump'} />
-      <div className="img-container">
-        {/* <Image className={`kuku ${jump}`} src={kuku} alt="kuku" /> */}
-        <button className="box flex items-center justify-center" onClick={animate}>
-          <Image src={box} alt="box" />
-        </button>
-        <Image className={`lid ${move} ${rotating} ${rotated}`} src={boxLid} alt="box-lid" />
-      </div>
-      <div className="flex flex-col items-center">
-        <Image src={gift.image} alt="Gift image" />
-        <p className=''>{gift.name}</p>
-      </div>
+      {gift.image ? (
+        <div className="flex flex-col items-center">
+          <Image src={gift.image} alt="Gift image" />
+          <p className=''>{gift.name}</p>
+        </div>
+      ) : (
+        <div className="img-container">
+        { /* <Image className={`kuku ${jump}`} src={kuku} alt="kuku" /> */}
+          <button className="box flex items-center justify-center" onClick={animate}>
+            <Image src={box} alt="box" />
+          </button>
+          <Image className={`lid ${move} ${rotating} ${rotated}`} src={boxLid} alt="box-lid" />
+        </div>
+      )}
     </div>
   )
 }
